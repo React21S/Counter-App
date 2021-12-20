bed0f697b40b8d4900f6c564977f9cd35bb31d00
FemiAdesola <75606768+FemiAdesola@users.noreply.github.com>
2021-12-20 15:23:32 +0200

    Added social media link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,28 @@
 const Footer = ()=>{
     return(
-      <div>
-        <footer>
-          <p>Femi Adesola &copy; 2021</p>   
-        </footer>
-      </div>
+      <div className="wrapper">
+            <footer>
+            <div className="footerWrapper">
+                
+                <div className="icons">
+                    <a href="https://www.linkedin.com/in/femi-adesola-oyinloye-106454145/" target="_blank" rel="noreferrer" className="linkedin">
+                            <i className="fab fa-linkedin fa-2x"></i>
+                        </a>
+                
+                        <a href="#1" className="facebook">
+                            <i className="fab fa-facebook fa-2x"></i>
+                        </a>
+
+                        <a href="https://github.com/FemiAdesola" target="_blank" rel="noreferrer" className="github">
+                            <i className="fab fa-github fa-2x"></i>
+                        </a>
+                    </div>
+                   
+                    <p>Femi Adesola &copy; 2021 </p>
+                    </div>
+                    
+            </footer>
+        </div>
     );
   };
 
